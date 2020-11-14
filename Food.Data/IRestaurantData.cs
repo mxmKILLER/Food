a1cf10c28890d8e23be7e9ae51770efa05c5f49d
Serhii Danylenko <2931190@gmail.com>
2020-11-14 21:21:52 +0200

    Project structure added

---
+++ Food.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using Food.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Food.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Garry`s Pizza", Location = "Lviv", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Mario`s Club", Location = "Lviv", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 3, Name = "La Costa", Location = "Lviv", Cuisine = CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(x => x.Name);
+        }
+    }
+}
