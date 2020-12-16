6249b7f57fc6b9f99f7a47bd161cbc104a46d3e0
Serhii Danylenko <2931190@gmail.com>
2020-12-16 17:49:50 +0200

    data tables added

--- Food/Startup.cs
+++ Food/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Food.Api;
 using Food.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -62,5 +63,6 @@ namespace Food
                 e.MapControllers();
             });
         }
+
     }
 }
